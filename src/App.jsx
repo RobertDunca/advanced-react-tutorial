d4d32be9cb5cbbfa9f0d43683ae641cd64e59af4
RobertDunca <duradi.mail@gmail.com>
2023-05-04 21:48:00 +0300

    custom hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // import Starter from './tutorial/'
 // import { StrictMode } from "react";
 // import About from "./tutorial/04-project-structure/starter/pages/About";
-import Starter from "./tutorial/07-useRef/starter/01-useRef-basics";
+import Starter from "./tutorial/08-custom-hooks/starter/02-fetch-data";
 // import Starter from "./tutorial/02-useEffect/starter/05-cleanup-function";
 
 function App() {
