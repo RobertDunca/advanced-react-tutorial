013fcecc953c85caadcc0c665546743a7b8e87a1
RobertDunca <duradi.mail@gmail.com>
2023-05-04 11:37:02 +0300

    forms part 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // import Starter from './tutorial/'
 // import { StrictMode } from "react";
 // import About from "./tutorial/04-project-structure/starter/pages/About";
-import Starter from "./tutorial/06-forms/starter/03-multiple-inputs";
+import Starter from "./tutorial/06-forms/starter/05-form-data";
 // import Starter from "./tutorial/02-useEffect/starter/05-cleanup-function";
 
 function App() {
--- src/tutorial/06-forms/starter/04-other-inputs.jsx
+++ src/tutorial/06-forms/starter/04-other-inputs.jsx
@@ -1,20 +1,42 @@
-import { useState } from 'react';
-const frameworks = ['react', 'angular', 'vue', 'svelte'];
+import { useState } from "react";
+const frameworks = ["react", "angular", "vue", "svelte"];
 const OtherInputs = () => {
+  const [shipping, setShipping] = useState(false);
+  const [framework, setFramework] = useState("react");
+
   return (
     <div>
-      <form className='form'>
+      <form className="form">
         <h4>Other Inputs</h4>
         {/* name */}
-        <div className='form-row' style={{ textAlign: 'left' }}>
-          <label htmlFor='shipping'> Free Shipping </label>
+        <div className="form-row" style={{ textAlign: "left" }}>
+          <label htmlFor="shipping"> Free Shipping </label>
+          <input
+            name="shipping"
+            type="checkbox"
+            id="shipping"
+            onChange={(e) => setShipping(e.target.checked)}
+            checked={shipping}
+          />
         </div>
-        <div className='form-row' style={{ textAlign: 'left' }}>
-          <label htmlFor='framework' className='form-label'>
+        <div className="form-row" style={{ textAlign: "left" }}>
+          <label htmlFor="framework" className="form-label">
             Framework
           </label>
+          <select
+            name="framework"
+            id="framework"
+            value={framework}
+            onChange={(e) => setFramework(e.target.value)}
+          >
+            {frameworks.map((fw) => (
+              <option key={fw} value={fw}>
+                {fw}
+              </option>
+            ))}
+          </select>
         </div>
-        <button type='submit' className='btn btn-block'>
+        <button type="submit" className="btn btn-block">
           submit
         </button>
       </form>
