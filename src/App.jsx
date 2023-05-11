d248596117c269e219f7de3fa1370ac12b7be592
RobertDunca <duradi.mail@gmail.com>
2023-05-11 17:29:03 +0300

    performance 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // import Starter from './tutorial/'
 // import { StrictMode } from "react";
 // import About from "./tutorial/04-project-structure/starter/pages/About";
-import Starter from "./tutorial/11-performance/starter/03-hooks";
+import Starter from "./tutorial/11-performance/starter/04-react-18";
 // import Starter from "./tutorial/02-useEffect/starter/05-cleanup-function";
 
 function App() {
